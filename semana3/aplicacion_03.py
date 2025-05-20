dc68a9db0061ad1ae5007c1e036d89fb2e1adf9a
Luis Diasgiron <189465404+lolitec@users.noreply.github.com>
2025-05-20 18:35:10 +0000

    UPDATED cambie el except de aplicacion03 para coincidir con las direcciones de pep8

--- semana3/aplicacion_03.py
+++ semana3/aplicacion_03.py
@@ -14,8 +14,10 @@ class Calculadora:
             # Imprime en la consola el texto Resultado y el valor de la division
             print(f"Resultado: {divisor/dividendo}")
             # Atrapa los errores posibles dentro del programa y los guarda en error
-        except (ZeroDivisionError, TypeError) as error:
+        except (ZeroDivisionError) as error:
             # Imprime en la consola el texto Error y el mensaje de error almacenado en error
+            print(f"Error: {error.args[0]}")
+        except (ZeroDivisionError) as error:
             print(f"Error: {error.args[0]}")
     
 mi_calculadora = Calculadora()
